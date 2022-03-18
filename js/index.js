3c5a0bb716627ccd8ee55e083004883b5e40314f
Franco Forna <ffornaa@gmail.com>
2022-03-18 19:04:35 -0300

    cambios en html y css

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+console.log(document.querySelector('.textStartBoxOne').textContent);
+
+// //Cambiar el contenido de la etiqueta html////
+// document.querySelector('.textStartBoxOne').textContent = `Correct number!`;
+
+document.querySelector('.score').textContent = 30;
+document.querySelector('.highScore').textContent = 101;

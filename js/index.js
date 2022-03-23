8177853cf69a2ba25c84da222abddf470e195c79
Franco Forna <ffornaa@gmail.com>
2022-03-23 17:19:22 -0300

    terminado con codigo limpio

--- js/index.js
+++ js/index.js
@@ -1,9 +1,68 @@
 'use strict';
 
-console.log(document.querySelector('.textStartBoxOne').textContent);
+// Setting values //////////////////////////////////////////////////////////////////
+let score = 20;
+let highScore = 0;
 
-// //Cambiar el contenido de la etiqueta html////
-// document.querySelector('.textStartBoxOne').textContent = `Correct number!`;
+//Function for indicator message//
+const displayMessage = function (message) {
+  document.querySelector('.textStartBoxOne').textContent = message;
+};
+//Function for score changing //////////////////////////////////////////////////////////////////
+const settingScore = function (value) {
+  document.querySelector('.score').textContent = value;
+};
+//Function for high score changing //////////////////////////////////////////////////////////////////
+const settingHighScore = function (value) {
+  document.querySelector('.highScore').textContent = value;
+};
+settingScore(score);
+settingHighScore(highScore);
 
-document.querySelector('.score').textContent = 30;
-document.querySelector('.highScore').textContent = 101;
+//Random Numnber /////
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+/// Check boton //////////////////////////////////
+document.querySelector('.buttonCheck').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.numberBox').value);
+
+  if (!guess) {
+    displayMessage('🛑 No number!');
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Thats the correct number!');
+    const showSecretNumber = (document.querySelector('.signal').textContent =
+      secretNumber);
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.signal').style.padding = '40px 60px';
+    settingScore(score);
+    if (score > highScore) {
+      highScore = score;
+      settingHighScore(highScore);
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '🤔 Try lower!' : '🙄 Try bigger!');
+      score--;
+      settingScore(score);
+    } else {
+      displayMessage('You loose 😢');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+/// Boton reseat //////////////////////////////////
+document.querySelector('.restartButton').addEventListener('click', function () {
+  const clear = (document.querySelector('body').style.backgroundColor =
+    'black');
+  document.querySelector('.signal').textContent = '?';
+  document.querySelector('.signal').style.padding = '20px 50px';
+  displayMessage('Start guessing...');
+  document.querySelector('.numberBox').value = '';
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  console.log(secretNumber);
+});
+
+console.log(secretNumber);
